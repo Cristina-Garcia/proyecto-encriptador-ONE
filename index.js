29da82e514b2b754344a1ddba59a1545705301e3
Cristina <gre.cristina20@gmail.com>
2023-05-15 15:44:27 -0600

    btn desncriptar y btn copy

--- index.js
+++ index.js
@@ -2,43 +2,74 @@ const encriptButton = document.querySelector("#btn-encript");
 const desencripButton = document.querySelector("#btn-desencript");
 
 const buttonCopy = document.querySelector("#button-copy");
+
 encriptButton.addEventListener("click", encript);
 desencripButton.addEventListener("click", desencript);
+buttonCopy.addEventListener("click", functionCopy);
 
 function encript() {
   const contenedor = document.querySelector(".text-encriptado");
   const areaButtonCopy = document.querySelector("#button-area-encriptado");
 
-  const input = document.querySelector("#input-area").value.toLowerCase();
-  const replaceWords = input
-    .replace(/a/g, "ai")
-    .replace(/e/g, "enter")
-    .replace(/i/g, "imes")
-    .replace(/o/g, "ober")
-    .replace(/u/g, "ufault");
-
+  let input = document.querySelector("#input-area").value.toLowerCase();
   console.log(input);
-  console.log(replaceWords);
-  document.getElementById("encriptado-principal").style = "display:none";
+  let matriz = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  for (let i = 0; i < matriz.length; i++) {
+    if (input.includes(matriz[i][0])) {
+      input = input.replaceAll(matriz[i][0], matriz[i][1]);
+      // console.log(input);
+    }
+  }
 
+  document.getElementById("encriptado-principal").style = "display:none";
   areaButtonCopy.style = "display:flex";
   contenedor.style = "display:flex";
   const encriptPhrase = document.createElement("p");
-  encriptPhrase.innerHTML = replaceWords;
+  encriptPhrase.classList.add("text-encrypt");
+  encriptPhrase.innerHTML = input;
   contenedor.appendChild(encriptPhrase);
-  input.value = "";
-  console.log(contenedor);
+  let cleanInput = document.querySelector("#input-area");
+  cleanInput.value = "";
 }
 
 function desencript() {
-  const input = document.querySelector("#input-area").value.toLowerCase();
+  const contenedor = document.querySelector(".text-encriptado");
+  let input = document.querySelector("#input-area").value.toLowerCase();
 
-  const message = input
-    .replace(/ai/g, "a")
-    .replace(/enter/g, "e")
-    .replace(/imes/g, "i")
-    .replace(/ober/g, "o")
-    .replace(/ufault/g, "u");
-  console.log(message);
+  let matriz = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  for (let i = 0; i < matriz.length; i++) {
+    if (input.includes(matriz[i][0])) {
+      input = input.replaceAll(matriz[i][1], matriz[i][0]);
+    }
+  }
+
+  const encriptPhrase = document.createElement("p");
+  encriptPhrase.classList.add("text-encrypt");
+  encriptPhrase.innerHTML = input;
+  contenedor.appendChild(encriptPhrase);
+  let cleanInput = document.querySelector("#input-area");
+  cleanInput.value = "";
 }
-//   const wordsEncript = ["ai", "enter", "imes", "ober", "ufalt"];
+
+function functionCopy() {
+  const textToDesencrypt = document.querySelector(".text-encrypt");
+
+  navigator.clipboard.writeText(textToDesencrypt.textContent);
+  textToDesencrypt.remove(".text-encrypt");
+  let cleanInput = document.querySelector("#input-area");
+  cleanInput.value = "";
+
+  // console.log(contenedor);
+}
